de748ee4528d5527ebfe2b416bad79fb9fd0d973
Dauda Ahmed Afolabi <55094632+dev-afolabi@users.noreply.github.com>
2021-04-24 23:21:38 +0100

    solved

---
+++ CountDuplicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DuplicateCount("Indivisibilities"));
+        }
+
+        public static int DuplicateCount(string str)
+        {
+            char[] temp = str.ToLower().ToCharArray();
+
+            Dictionary<char,int> dict = new Dictionary<char, int>();
+            int maxCount = 0;
+            foreach (var item in temp)
+            {
+                if(dict.ContainsKey(item))
+                {
+                    ++dict[item];
+                }
+                else{
+                    dict[item] = 1;
+                }
+            }
+            foreach (var item in dict.Values)
+            {
+                if(item > 1)
+                    ++maxCount;
+            }
+
+            return maxCount;
+        }
+    }
+}
---
+++ sumMix/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace sumMix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumMix(new object[] {9, 3, "7", "3"}));
+        }
+
+        public static int SumMix(object[] x)
+        {
+            return x.Select(c => int.Parse(c.ToString())).Sum();
+        }
+    }
+}
