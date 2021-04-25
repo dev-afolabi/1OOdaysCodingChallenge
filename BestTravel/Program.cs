ba6f22cbfb2b38be62d740315576dcb615d16ce2
Dauda Ahmed Afolabi <55094632+dev-afolabi@users.noreply.github.com>
2021-04-25 23:01:32 +0100

    day 7

---
+++ BestTravel/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestTravel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int k = 3;
+            var n = new[] { "1", "2", "3", "4", "5" };
+
+            Console.Write("n: ");
+            foreach (var item in n)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("k: {0}", k);
+            Console.WriteLine();
+
+            foreach (IEnumerable<string> i in Combinations(n, k))
+                Console.WriteLine(string.Join(" ", i));
+        }
+        public static int FindDifference(int[] a, int[] b)
+        {
+            //loading...
+            return -1;
+        }
+
+
+        private static bool NextCombination(IList<int> num, int n, int k)
+        {
+            bool finished;
+
+            var changed = finished = false;
+
+            if (k <= 0) return false;
+
+            for (var i = k - 1; !finished && !changed; i--)
+            {
+                if (num[i] < n - 1 - (k - 1) + i)
+                {
+                    num[i]++;
+
+                    if (i < k - 1)
+                        for (var j = i + 1; j < k; j++)
+                            num[j] = num[j - 1] + 1;
+                    changed = true;
+                }
+                finished = i == 0;
+            }
+
+            return changed;
+        }
+
+        private static IEnumerable Combinations<T>(IEnumerable<T> elements, int k)
+        {
+            var elem = elements.ToArray();
+            var size = elem.Length;
+
+            if (k > size) yield break;
+
+            var numbers = new int[k];
+
+            for (var i = 0; i < k; i++)
+                numbers[i] = i;
+
+            do
+            {
+                yield return numbers.Select(n => elem[n]);
+            } while (NextCombination(numbers, size, k));
+        }
+    }
+}
---
+++ FindDifference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int FindDifference(int[] a, int[] b)
+        {
+            //loading...
+            int sumA = 0;
+            int sumB = 0;
+
+            foreach (var item in a)
+            {
+                sumA+=item;
+            }
+            foreach (var item in b)
+            {
+                sumB+=item;
+            }
+            return Math.Abs(sumA - sumB);
+        }
+    }
+}
