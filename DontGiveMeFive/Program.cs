687598adf59fa5cb1707684da410b5c37b3bc1f4
Dauda Ahmed Afolabi <55094632+dev-afolabi@users.noreply.github.com>
2021-04-26 22:00:05 +0100

    day-8

---
+++ DontGiveMeFive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DontGiveMeFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int DontGiveMeFive(int start, int end)
+        {
+
+            List<int> values = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i.ToString().Contains("5"))
+                {
+                    continue;
+                }
+                values.Add(i);
+            }
+            return values.Count;
+        }
+    }
+}
---
+++ NumberWithDigitsInside/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberWithDigitsInside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumbersWithDigitInside(11, 1));
+        }
+        public static long[] NumbersWithDigitInside(long x, long d)
+        {
+            long[] result = new long[3];
+            List<long> values = new List<long>(); 
+
+            for (int i = 1; i <= x; i++)
+            {
+                if(i.ToString().Contains(d.ToString())  )
+                {
+                    values.Add(i);
+                }
+            }
+            if(values.Count == 0)
+                return result;
+            result[0] = values.Count;
+            result[1] = values.Sum();
+            result[2] = values.Aggregate((acc, val) => acc * val);
+            return result;
+        }
+    }
+}
