f6ac279f48d92e0a39f22592da5aba3898935501
Dauda Ahmed Afolabi <55094632+dev-afolabi@users.noreply.github.com>
2021-04-27 23:29:09 +0100

    day 9

---
+++ DisemVowel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace DisemVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Disemvowel("Hello World!"));
+        }
+        public static string Disemvowel(string str)
+        {
+            string vowels = "aeiouAEIOU";
+            return String.Join("",str.ToLower().Where(c => !vowels.Contains(c)).ToArray());
+        }
+    }
+}
---
+++ Mirrors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Mirrors
+{
+    class Program
+    {
+        delegate string del (string n);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    
+        public static string VertMirror(string strng)
+        {
+            string value ="";
+            foreach (var item in strng.Split('\n'))
+            {
+                value += item.Reverse();
+                value += '\n';
+            }
+            return value.TrimEnd('\n');
+        
+        }
+
+        
+        public static string HorMirror(string strng)
+        {
+            string value = string.Join('\n',strng.Split('\n').Reverse());
+
+            return value.TrimEnd('\n');
+        }
+
+        public static string Oper(, string s)
+        {
+            // your code and complete ... before operator
+        }
+    }
+}
